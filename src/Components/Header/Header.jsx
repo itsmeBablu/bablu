48c6ed2306467bd852dea95479c78362e2f74e55
bablu <kanala.babureddy@gmail.com>
2022-05-29 15:54:00 +0200

    morebtnRemove

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import './header.css'
-import CTA from './CTA'
-import ME from '../../assets/me.png'
-import HeaderSocials from './HeaderSocials'
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/me.png";
+import HeaderSocials from "./HeaderSocials";
 export default function Header() {
   return (
     <header>
-      <div  className='container header__container'>
+      <div className="container header__container">
         <h3>नमस्ते I'm</h3>
         <h1>Bablu Reddy</h1>
-        <h5 className='text-light'>BIM Engineer/Developer</h5>
+        <h5 className="text-light">BIM Engineer/Developer</h5>
         <CTA />
-        <HeaderSocials/>
+        <HeaderSocials />
         <div className="me">
           <img src={ME} alt="me" />
         </div>
-        <a href="#contact" className='scroll__down'>Scroll Down</a>
+        {/* <a href="#contact" className='scroll__down'>Scroll Down</a> */}
       </div>
     </header>
-  )
+  );
 }
--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,81 +1,78 @@
-import React from 'react';
-import './testimonials.css'
-import DL from '../../assets/dl1.jpg'
-import IVFX from '../../assets/ivfx.jpg'
-import IBV from '../../assets/ibv.png'
-
+import React from "react";
+import "./testimonials.css";
+import DL from "../../assets/dl1.jpg";
+import IVFX from "../../assets/ivfx.jpg";
+import IBV from "../../assets/ibv.png";
 
 // import Swiper core and required modules
-import { Pagination } from 'swiper';
+import { Pagination } from "swiper";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Testimonials = () => {
+  const data = [
+    {
+      avatar: IBV,
+      name: "IBV",
+      Duration: "from June 2019",
+      Role: "BIM Engineer",
+      link: "https://www.ibv-hd.de/",
+      review:
+        " Here I’m working as a BIM engineer, and specifically on Heat Load calculations for commercial and residential projects. IBV is one of the oldest and leading engineering offices for supply technology in the Rhine-Neckar metropolitan region.Provides range of services including consulting, planning and construction supervision of all trades of technical building equipment.",
+    },
+    {
+      avatar: IVFX,
+      name: "Infinito VFX",
+      Role: "Game Developer",
+      Duration: "June 2018 to Dec 2018",
+      link: "https://www.infinitovfx.com/",
+      review:
+        "I worked as an AR/VR Developer, in other ways as a Game Developer for a short period.InfinitoVFX is a startup and provides AR/VR, 2d Animations and Graphic designing Projects, based on client requirement.",
+    },
+    {
+      avatar: DL,
+      name: "Digital Lync",
+      Role: "3D Visualizer/Artist",
+      link: "https://www.digital-lync.com/",
+      Duration: "December 2015 to June 2018",
+      review:
+        "Here, I worked as a 3d Visualizer/Developer.Previously it used to be a IT company, later on that changed to  Skill Up training Institute.Enlightening environment with flexible things dealing with Projects and students.I even trained a few students in UI/UX skill up Programming.",
+    },
+  ];
+  return (
+    <section id="testimonials">
+      <h5>About the</h5>
+      <h2>Companies that, I worked and working</h2>
 
-    const data = [
-        {
-            avatar:IBV,
-            name:'IBV',
-            Duration: 'from June 2019',
-            Role:'BIM Engineer',
-            link:'https://www.ibv-hd.de/',
-            review: 'Enjoy the flexibility of selecting online or offline classes with Digital-Lync first-ever hybrid learning model. Get the fruitful chance of choosing between the privilege of learning from home or the advantage of one-on-one knowledge gaining - all in one place.'
-        },
-        {
-            avatar:IVFX,
-            name:'Infinito VFX',
-            Role:'Game Developer',
-            Duration: 'June 2018 to Dec 2018',
-            link:'https://www.infinitovfx.com/',
-            review: 'Enjoy the flexibility of selecting online or offline classes with Digital-Lync first-ever hybrid learning model. Get the fruitful chance of choosing between the privilege of learning from home or the advantage of one-on-one knowledge gaining - all in one place.'
-        },
-        {
-            avatar:DL,
-            name:'Digital Lync',
-            Role:'3D Visualizer/Artist',
-            link:'https://www.digital-lync.com/',
-            Duration: 'December 2015 to June 2018',
-            review: 'Enjoy the flexibility of selecting online or offline classes with Digital-Lync first-ever hybrid learning model. Get the fruitful chance of choosing between the privilege of learning from home or the advantage of one-on-one knowledge gaining - all in one place.'
-        }
-
-    ]
-    return (
-        <section id = 'testimonials'>
-            <h5>About</h5>
-            <h2>Companies that, I worked</h2>
-
-            <Swiper 
-                className="container testimonials__container"
-                modules={[ Pagination]}
-                spaceBetween={40}
-                slidesPerView={1}
-                pagination={{ clickable: true }}>
-                {
-                data.map(({avatar,name,Role,link,Duration,review},index) => {
-                    return(
-                        <SwiperSlide key = {index} className="testimonials">
-                        <div className="client__avatar">
-                            <a href={link} target='_blank'><img src={avatar} alt=""/></a>
-                        </div>
-                        <h3 className="client__name">{name}</h3>
-                        <h5 className="client__name">{Duration}</h5>
-                        <h3 className="client__role">{Role}</h3>
-                        <small className="client__review">
-                            {review}
-                        </small>
-                    </SwiperSlide>      
-                    )
-                })
-            }   
-                
-            </Swiper>
-        </section>
-    );
-}
+      <Swiper
+        className="container testimonials__container"
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ avatar, name, Role, link, Duration, review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonials">
+              <div className="client__avatar">
+                <a href={link} target="_blank">
+                  <img src={avatar} alt="" />
+                </a>
+              </div>
+              <h3 className="client__name">{name}</h3>
+              <h5 className="client__name">{Duration}</h5>
+              <h3 className="client__role">{Role}</h3>
+              <small className="client__review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
+};
 
 export default Testimonials;
